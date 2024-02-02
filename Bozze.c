190fd4ad4c9fa57de6b42b3edc6e361ecf277c20
favomatte <158043315+favomatte@users.noreply.github.com>
2024-02-02 10:21:07 +0100

    Update Bozze.c

--- Bozze.c
+++ Bozze.c
@@ -5,4 +5,12 @@ int main()
     int ricerca;
     int funzioni;
 }
---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------------
+Situata a
+Roma,.
+Luiss
+specializzata nelle scienze sociali.
+Con i suoi 4 Dipartimenti: Impresa e Management,
+Economia e
+Finanza, Giurisprudenza e Scienze Politiche, offre ad oltre 10.000 studenti un ambiente dinamico e internazionale.
+----------------------------------------------
